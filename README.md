896f75b1873ae9db2b4f21bb0a3b673aa831b5c6
wyne-ybanez <wyne_01@icloud.com>
2021-10-12 23:36:22 +0100

    Add filtering expense functionality

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # React Budget Planner 
 
+## Extra Challenges 
+
+- Expense Filter
+- Edit budget
+
 ## Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,15 +1,41 @@
 import ExpenseItem from './ExpenseItem'
 import { AppContext } from '../context/AppContext'
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 const ExpenseList = () => {
   const { expenses } = useContext(AppContext)
+
+  const [filteredExpenses, setfilteredExpenses] = useState(expenses || [])
+
+  useEffect(() => {
+    setfilteredExpenses(expenses)
+  }, [expenses])
+
+  const handleChange = (event) => {
+    const searchResult = expenses.filter((filteredExpense) =>
+      filteredExpense.name.toLowerCase().includes(event.target.value)
+    )
+    setfilteredExpenses(searchResult)
+  }
+
   return (
-    <ul className="list-group">
-      {expenses.map((expense) => (
-        <ExpenseItem id={expense.id} name={expense.name} cost={expense.cost} />
-      ))}
-    </ul>
+    <div>
+      <input
+        type="text"
+        class="form-control mb-2 mr-sm-2"
+        placeholder="Type to search..."
+        onChange={handleChange}
+      />
+      <ul className="list-group">
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            id={expense.id}
+            name={expense.name}
+            cost={expense.cost}
+          />
+        ))}
+      </ul>
+    </div>
   )
 }
 
